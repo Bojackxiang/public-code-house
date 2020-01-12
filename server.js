73b13d19b61bc6e25ba5621682429d8edb189b0b
alex <batmandocode@gmail.com>
2020-01-12 21:47:13 +1100

    load the nextjs to koa

---
+++ server.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const next = require('next')
+const Router = require('koa-router')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = new Koa()
+    const router = new Router()
+
+    // ! 下面这一段代码，决定了next项目是否可以挂载到 koa 上面
+    server.use(async (ctx, next) => {
+        await handle(ctx.req, ctx.res)
+        ctx.response = false
+    })
+
+    server.use(router.routes())
+    server.listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
